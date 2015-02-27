3201fd41c351f9bc1f9a50b0964ee5a1d3bcdc94
Charles Meyer <charles.meyer@tufts.edu>
2015-02-27 12:27:11 -0500

    added dewick and carm urls

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import webapp2
 import urllib2
 
+DINING_HALL_URLS = ["http://menus.tufts.edu/foodpro/shortmenu.asp?sName=Tufts+Dining&locationNum=11&locationName=Dewick+MacPhie+Dining+Center&naFlag=1",
+                    "http://menus.tufts.edu/foodpro/shortmenu.asp?sName=Tufts+Dining&locationNum=09&locationName=Carmichael+Dining+Center&naFlag=1"]
+
+
 class API(webapp2.RequestHandler):
     def get(self):
         self.response.headers["Content-Type"] = "text/json"
@@ -8,7 +12,7 @@ class API(webapp2.RequestHandler):
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('they have food')
+        self.response.write('<h1>API access @ /API</h1> <a href = "https://github.com/charles-meyer/">click if questions</a>')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
