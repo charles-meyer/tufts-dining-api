a11955477f6b1ec32ef2af1e3fd2705f91f79912
Eric Bailey <popcorncolonel@gmail.com>
2015-02-27 12:42:55 -0500

    api skele there

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 import webapp2
 import urllib2
+import json
 
 class API(webapp2.RequestHandler):
-    def get(self):
+    def get(self, dining_hall=None):
+        self.response.headers["Access-Control-Allow-Origin"] = "*"
         self.response.headers["Content-Type"] = "text/json"
-        self.response.write('{food:food}')
+        d = {}
+        d['food'] = 'food'
+        if dining_hall:
+            d['dining_hall'] = dining_hall
+        str_rep = json.dumps(d, separators=(',',':'))
+        self.response.write(str_rep)
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
+        self.response.headers["Access-Control-Allow-Origin"] = "*"
         self.response.write('they have food')
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-    ('/api', API)
+    (r'/', MainHandler),
+    (r'/api', API),
+    (r'/api/(dewick|carm)', API)
 ], debug=True)
 
