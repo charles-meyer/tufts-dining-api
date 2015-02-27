da539452631e1e73116968a715a39582eeb951ef
Charles Meyer <charles.meyer@tufts.edu>
2015-02-27 12:32:08 -0500

    got dining responses

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import webapp2
 import urllib2
 
-DINING_HALL_URLS = ["http://menus.tufts.edu/foodpro/shortmenu.asp?sName=Tufts+Dining&locationNum=11&locationName=Dewick+MacPhie+Dining+Center&naFlag=1",
-                    "http://menus.tufts.edu/foodpro/shortmenu.asp?sName=Tufts+Dining&locationNum=09&locationName=Carmichael+Dining+Center&naFlag=1"]
-
+DEWICK_URL = "http://menus.tufts.edu/foodpro/shortmenu.asp?sName=Tufts+Dining&locationNum=11&locationName=Dewick+MacPhie+Dining+Center&naFlag=1"
+CARM_URL = "http://menus.tufts.edu/foodpro/shortmenu.asp?sName=Tufts+Dining&locationNum=09&locationName=Carmichael+Dining+Center&naFlag=1"
 
 class API(webapp2.RequestHandler):
     def get(self):
         self.response.headers["Content-Type"] = "text/json"
         self.response.write('{food:food}')
+        dewick_page = urllib2.urlopen(DEWICK_URL).open
+        carm_page = urllib2.urlopen(CARM_URL).open
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
